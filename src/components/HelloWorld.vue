bbbf111f72ff7868e4a6766d4727b1878766b857
Aditya J <ajain15@uic.edu>
2018-01-27 19:41:59 -0500

    getting vue to play well with three

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,95 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <canvas :width=windowWidth :height=windowHeight ref="ccontain"/>
 </template>
 
 <script>
+import * as THREE from 'three'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight
     }
+  },
+  methods:{
+    windowResize: function(){
+      this.windowWidth = window.innerWidth
+      this.windowHeight = window.innerHeight
+    }
+  },
+  mounted: function () {
+    window.addEventListener('resize', this.windowResize)
+    var scene = new THREE.Scene()
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+    var renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.ccontain
+    })
+    var geometry = new THREE.BoxGeometry(1, 1, 1)
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    var cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+
+    camera.position.z = 5
+
+    var animate = function () {
+      requestAnimationFrame(animate)
+      cube.rotation.x += 0.1
+      cube.rotation.y += 0.1
+      renderer.render(scene, camera)
+    }
+
+    animate()
   }
 }
 </script>
